5ce8fd9ef4e4ff6c1d3bfc06a0c94bfd065f220d
Mohammad Samiul Islam <forthright48@gmail.com>
2016-06-20 23:02:55 +0600

    Added eslint

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 (function() {
   'use strict';
 
-  var app = require('express')();
-  var http = require('http').Server(app);
-  var io = require('socket.io')(http);
+  const app = require('express')();
+  const http = require('http').createServer(app);
+  const io = require('socket.io')(http);
 
   app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(`${__dirname}/index.html`);
   });
 
   io.on('connection', function(socket) {
@@ -24,5 +24,4 @@
   http.listen(3000, function() {
     console.log('listening on *:3000');
   });
-
 }());
